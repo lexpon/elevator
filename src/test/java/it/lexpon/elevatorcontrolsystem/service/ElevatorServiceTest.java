205840dd3f38fe41da1a6b652a9d20346de567eb
Alex Ponomarenko <alex@lexpon.de>
2020-09-14 23:23:16 +0200

    Add unit test for ElevatorService

--- src/test/java/it/lexpon/elevatorcontrolsystem/service/ElevatorServiceTest.java
+++ src/test/java/it/lexpon/elevatorcontrolsystem/service/ElevatorServiceTest.java
@@ -1,6 +1,7 @@
 package it.lexpon.elevatorcontrolsystem.service;
 
 import static it.lexpon.elevatorcontrolsystem.domainobject.Direction.*;
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
@@ -8,12 +9,13 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import it.lexpon.elevatorcontrolsystem.datatransferobject.ElevatorStatusResponse;
+import it.lexpon.elevatorcontrolsystem.datatransferobject.PickupRequest;
 import it.lexpon.elevatorcontrolsystem.domainobject.Elevator;
 
 
 public class ElevatorServiceTest {
 
-	private final List<Integer> elevatorIds = List.of(1);
+	private final List<Integer> elevatorIds = List.of(16);
 
 	private final ElevatorService testee = new ElevatorService(elevatorIds);
 
@@ -32,6 +34,29 @@ public class ElevatorServiceTest {
 		expectedElevator.changeDirection(NONE);
 		expectedElevator.changeFloor(0);
 		assertThat(elevators.get(0)).isEqualToComparingFieldByField(expectedElevator);
+	}
+
+
+	@Test
+	public void shouldAcceptPickupRequest() {
+		// GIVEN
+		PickupRequest request = PickupRequest.builder()
+			.currentFloor(1)
+			.destinationFloor(5)
+			.build();
+
+		// WHEN
+		testee.pickup(request);
+
+		// THEN
+		ElevatorStatusResponse status = testee.getStatus();
+		List<Elevator> elevatorsWithOpenRequests = status.getElevators().stream()
+			.filter(elevator -> elevator.getPickupRequestsOpen().size() > 0)
+			.collect(toList());
+		assertThat(elevatorsWithOpenRequests.size()).isEqualTo(1);
+		Elevator elevatorWithRequest = elevatorsWithOpenRequests.get(0);
+		assertThat(elevatorWithRequest.getPickupRequestsOpen().size()).isEqualTo(1);
+		assertThat(elevatorWithRequest.getPickupRequestsOpen().get(0)).isEqualTo(request);
 	}
 
 }
